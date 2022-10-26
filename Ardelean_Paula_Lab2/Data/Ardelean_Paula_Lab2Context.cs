3e553c94a3458802101cf76179f0d7f979ae9101
DELL <DELL@DESKTOP-760U628>
2022-10-26 15:46:42 +0300

    Add project files.

---
+++ Ardelean_Paula_Lab2/Data/Ardelean_Paula_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ardelean_Paula_Lab2.Models;
+
+namespace Ardelean_Paula_Lab2.Data
+{
+    public class Ardelean_Paula_Lab2Context : DbContext
+    {
+        public Ardelean_Paula_Lab2Context (DbContextOptions<Ardelean_Paula_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ardelean_Paula_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Ardelean_Paula_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Ardelean_Paula_Lab2/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ardelean_Paula_Lab2.Data;
+using Ardelean_Paula_Lab2.Models;
+
+namespace Ardelean_Paula_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ardelean_Paula_Lab2.Data.Ardelean_Paula_Lab2Context _context;
+
+        public IndexModel(Ardelean_Paula_Lab2.Data.Ardelean_Paula_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book
+                    .Include(b => b.Publisher)
+                    .ToListAsync();
+            }
+        }
+    }
+}
