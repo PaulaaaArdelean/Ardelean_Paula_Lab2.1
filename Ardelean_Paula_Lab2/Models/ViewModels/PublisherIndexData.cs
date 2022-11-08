af8e3913270f80e450a3388d317586c82d90ef9d
DELL <DELL@DESKTOP-760U628>
2022-11-08 15:48:58 +0200

    Sfarsit lab4

---
+++ Ardelean_Paula_Lab2/Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Ardelean_Paula_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
