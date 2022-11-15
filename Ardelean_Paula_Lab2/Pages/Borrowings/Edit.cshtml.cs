07fbe525148683665f67f58b65d9b2ba1fc6b4c5
DELL <DELL@DESKTOP-760U628>
2022-11-15 17:48:33 +0200

    Sfarsit Lab5 +Tema

--- Ardelean_Paula_Lab2/Pages/Borrowings/Edit.cshtml.cs
+++ Ardelean_Paula_Lab2/Pages/Borrowings/Edit.cshtml.cs
@@ -36,8 +36,12 @@ namespace Ardelean_Paula_Lab2.Pages.Borrowings
                 return NotFound();
             }
             Borrowing = borrowing;
-           ViewData["BookID"] = new SelectList(_context.Book, "ID", "ID");
-           ViewData["MemberID"] = new SelectList(_context.Set<Member>(), "ID", "ID");
+            var bookList = _context.Book
+            .Include(b => b.Author)
+            .Select(x => new { x.ID, BookFullName = x.Title + " - " + x.Author.LastName + " " + x.Author.FirstName });
+
+           ViewData["BookID"] = new SelectList(bookList, "ID", "BookFullName");
+           ViewData["MemberID"] = new SelectList(_context.Set<Member>(), "ID", "FullName");
             return Page();
         }
 
